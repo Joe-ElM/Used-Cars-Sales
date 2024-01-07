cbe3278db69d0885fc20d3f20039604b556644b1
joe el <asome.joe123@gmail.com>
2024-01-07 04:30:43 +0100

    GridSearch of LR and KNN have been added

--- Notebooks/Functions.py
+++ Notebooks/Functions.py
@@ -195,3 +195,33 @@ def CLEAN_All_DATA(df, X_aim):
    
     return X_train, X_test, y_train, y_test, X_aim
 
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+def Confusion_Matrix_Func(y_test, y_pred, model_name):
+    fig, ax = plt.subplots(figsize=[10, 6])
+
+# -------------------------------------------------------------------------------------------------
+    cf_matrix     = confusion_matrix(y_test, y_pred)
+    group_names   = ['True Negative', 'False Positive', 'False Negative', 'True Positive']
+    group_counts  = ['{0:0.0f}'.format(value) for value in cf_matrix.flatten()]
+    group_percent = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+
+    labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in zip(group_names, group_counts, group_percent)]
+    labels = np.asarray(labels).reshape(2, 2)
+
+    # ----------------------------------------------------------------------------------------------
+    sns.heatmap(cf_matrix,
+                annot     = labels,
+                ax        = ax,
+                annot_kws = {'size': 13},
+                cmap      = 'Blues',
+                fmt       = '')
+
+    ax.set_xlabel('Prediction')
+    ax.set_ylabel('Truth')
+    ax.xaxis.set_ticklabels(['G-Buy', 'Lemon'])
+    ax.yaxis.set_ticklabels(['G-Buy', 'Lemon'])
+    ax.set_title(f'Confusion Matrix of {model_name}\n')
+    
+    cf_matrix, fig
+
